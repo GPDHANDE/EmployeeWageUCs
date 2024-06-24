5eed0d22d8720305a1cc112940b181dab915c1fd
Gaurav <gpdhande12@gmail.com>
2024-06-24 18:52:47 +0530

    printed welcome messege

---
+++ EmployeeWageUC.java
@@ -0,0 +1,10 @@
+package org.EmpWage;
+
+public class EmployeeWageUC {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to EmployeeWage Computation Program on Master Branch");
+	}
+
+}
