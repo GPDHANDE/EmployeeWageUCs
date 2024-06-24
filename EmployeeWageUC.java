673f4d3eb6a22e2df28fc0916c144f32fde1c0dc
Gaurav <gpdhande12@gmail.com>
2024-06-25 00:47:21 +0530

    calculates monthly wage of an emp

--- EmployeeWageUC.java
+++ EmployeeWageUC.java
@@ -1,28 +1,28 @@
 package org.EmpWage;
 
-public class EmployeeWageUC {
+public class EmployeeWageUC 
+{
+	static final int isFullTime = 2;
+	static final int isPartTime= 1;
+	static final int wagePerHr = 20 ;
 
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome to EmployeeWage Computation Program");
-		int isFullTime = 2;
-		int isPartTime= 1;
-		int wagePerHr = 20 ;
-		int dayHrFulltime =8;
-		int dayHrPartTime =4;
+		int dayHrFulltime =8 ,dayHrPartTime =4 , totalWage = 0,noOfDays=10;
 		
 		int empCheckAttendance = (int) (Math.floor(Math.random()*10)%3);
-		
 		switch(empCheckAttendance)
+			
 		{
-			case 1:  int dailyWageOfEmp = wagePerHr*dayHrFulltime;
-			System.out.println("Daily Employee wage of an Full time Employee is "+dailyWageOfEmp+"");
-			break;
-			
-			case 2: int dailyWageOfEmp1 = wagePerHr*dayHrPartTime;
-			System.out.println("Daily Employee wage of an Full time Employee is "+dailyWageOfEmp1+"");
-			break;
-			
+			case 1:  int dailyWageOfEmp = wagePerHr*dayHrFulltime*noOfDays;
+					 System.out.println("Daily Employee wage of an Full time Employee is "+dailyWageOfEmp+"");
+					break;
+				
+			case 2: int dailyWageOfEmp1 = wagePerHr*dayHrPartTime*noOfDays;
+					System.out.println("Daily Employee wage of an Full time Employee is "+dailyWageOfEmp1+"");
+					break;
+				
 			default : System.out.println("Employee is absent");
 		}
 		 
