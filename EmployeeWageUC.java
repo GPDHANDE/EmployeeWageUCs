3f05205c9982155beb1a995f7ae6c7c7cf051037
Gaurav <gpdhande12@gmail.com>
2024-06-25 00:10:09 +0530

    checks part time empwage

--- EmployeeWageUC.java
+++ EmployeeWageUC.java
@@ -5,15 +5,22 @@ public class EmployeeWageUC {
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome to EmployeeWage Computation Program");
-		int present = 1;
+		int isFullTime = 2;
+		int isPartTime= 1;
 		int wagePerHr = 20 ;
-		int dayHr =8;
+		int dayHrFulltime =8;
+		int dayHrPartTime =4;
 		
-		double empCheckAttendance = Math.floor(Math.random()*10)%2;
-		if(empCheckAttendance==present)
+		double empCheckAttendance = Math.floor(Math.random()*10)%3;
+		if(empCheckAttendance==isFullTime)
 		{
-			int dailyWageOfEmp = wagePerHr*dayHr;
-			System.out.println("Daily Employee wage of an Employee is "+dailyWageOfEmp+"");
+			int dailyWageOfEmp = wagePerHr*dayHrFulltime;
+			System.out.println("Daily Employee wage of an Full time Employee is "+dailyWageOfEmp+"");
+		}
+		else if(empCheckAttendance== isPartTime)
+		{
+			int dailyWageOfEmp = wagePerHr*dayHrPartTime;
+			System.out.println("Daily Employee wage of an part time Employee is "+dailyWageOfEmp+"");
 		}
 		else
 		{
